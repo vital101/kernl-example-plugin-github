808c312a2f66bf2a65dee07e4c7f7e23b26c99fb
Jack Slingerland <jack.slingerland@gmail.com>
2017-05-23 20:23:44 -0400

    Test for new logo

--- kernl-example-plugin.php
+++ kernl-example-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Kernl Example Plugin GitHub
  * Plugin URI: https://kernl.us
  * Description: The Kernl Plugin for testing GitHub Testing
- * Version: 2.0.6
+ * Version: 2.0.7
  * Author: Jack Slingerland
  * Author URI: http://re-cycledair.com
  */

45b377fedbd01c0a1d6d7f7ecb9b3d9f83543314
Jack Slingerland <jack.slingerland@gmail.com>
2022-04-16 19:35:17 -0400

    Node16 tests

--- kernl-example-plugin-github.php
+++ kernl-example-plugin-github.php
@@ -3,7 +3,7 @@
  * Plugin Name: Kernl Example Plugin GitHub
  * Plugin URI: https://kernl.us
  * Description: The Kernl Plugin for testing GitHub Testing
- * Version: 2.9.2
+ * Version: 2.9.3
  * Author: Jack Slingerland
  * Author URI: http://re-cycledair.com
  */
